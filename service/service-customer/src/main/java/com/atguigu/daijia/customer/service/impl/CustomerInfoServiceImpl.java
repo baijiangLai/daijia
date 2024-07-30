552eaf79732977005c156f66c7bd2c47493cc025
laibaijiang <laibaijiang@laibaijiang.com>
2024-07-30 14:31:22 +0800

    乘客端小程序授权登录

---
+++ service/service-customer/src/main/java/com/atguigu/daijia/customer/service/impl/CustomerInfoServiceImpl.java
@@ -0,0 +1,68 @@
+package com.atguigu.daijia.customer.service.impl;
+
+import cn.binarywang.wx.miniapp.api.WxMaService;
+import cn.binarywang.wx.miniapp.bean.WxMaJscode2SessionResult;
+import com.atguigu.daijia.common.execption.GuiguException;
+import com.atguigu.daijia.common.result.ResultCodeEnum;
+import com.atguigu.daijia.customer.mapper.CustomerInfoMapper;
+import com.atguigu.daijia.customer.mapper.CustomerLoginLogMapper;
+import com.atguigu.daijia.customer.service.CustomerInfoService;
+import com.atguigu.daijia.model.entity.customer.CustomerInfo;
+import com.atguigu.daijia.model.entity.customer.CustomerLoginLog;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class CustomerInfoServiceImpl extends ServiceImpl<CustomerInfoMapper, CustomerInfo> implements CustomerInfoService {
+
+    @Autowired
+    private WxMaService wxMaService;
+
+    @Autowired
+    private CustomerInfoMapper customerInfoMapper;
+
+    @Autowired
+    private CustomerLoginLogMapper customerLoginLogMapper;
+
+    /**
+     * 条件：
+     *      1、前端开发者appid与服务器端appid一致
+     *      2、前端开发者必须加入开发者
+     * @param code
+     * @return
+     */
+    @Override
+    public Long login(String code) {
+        String openId = null;
+        try {
+            //获取openId
+            WxMaJscode2SessionResult sessionInfo = wxMaService.getUserService().getSessionInfo(code);
+            openId = sessionInfo.getOpenid();
+            log.info("【小程序授权】openId={}", openId);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new GuiguException(ResultCodeEnum.DATA_ERROR);
+        }
+
+        CustomerInfo customerInfo = customerInfoMapper.selectOne(new LambdaQueryWrapper<CustomerInfo>().eq(CustomerInfo::getWxOpenId, openId));
+        if(null == customerInfo) {
+            customerInfo = new CustomerInfo();
+            customerInfo.setNickname(String.valueOf(System.currentTimeMillis()));
+            customerInfo.setAvatarUrl("https://oss.aliyuncs.com/aliyun_id_photo_bucket/default_handsome.jpg");
+            customerInfo.setWxOpenId(openId);
+            customerInfoMapper.insert(customerInfo);
+        }
+
+        //登录日志
+        CustomerLoginLog customerLoginLog = new CustomerLoginLog();
+        customerLoginLog.setCustomerId(customerInfo.getId());
+        customerLoginLog.setMsg("小程序登录");
+        customerLoginLogMapper.insert(customerLoginLog);
+        return customerInfo.getId();
+    }
+}
---
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/service/CustomerService.java
@@ -0,0 +1,7 @@
+package com.atguigu.daijia.customer.service;
+
+public interface CustomerService {
+
+
+    String login(String code);
+}
