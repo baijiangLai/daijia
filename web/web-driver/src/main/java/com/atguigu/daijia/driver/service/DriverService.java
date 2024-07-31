2afb602f4660f45dd3d0fa964e930d5b09526190
laibaijiang <laibaijiang@laibaijiang.com>
2024-07-31 15:57:54 +0800

    司机修改认证信息相关接口

--- web/web-driver/src/main/java/com/atguigu/daijia/driver/service/DriverService.java
+++ web/web-driver/src/main/java/com/atguigu/daijia/driver/service/DriverService.java
@@ -1,5 +1,7 @@
 package com.atguigu.daijia.driver.service;
 
+import com.atguigu.daijia.model.form.driver.UpdateDriverAuthInfoForm;
+import com.atguigu.daijia.model.vo.driver.DriverAuthInfoVo;
 import com.atguigu.daijia.model.vo.driver.DriverLoginVo;
 
 public interface DriverService {
@@ -8,4 +10,8 @@ public interface DriverService {
     String login(String code);
 
     DriverLoginVo getDriverLoginInfo();
+
+    DriverAuthInfoVo getDriverAuthInfo(Long driverId);
+
+    Boolean updateDriverAuthInfo(UpdateDriverAuthInfoForm updateDriverAuthInfoForm);
 }
