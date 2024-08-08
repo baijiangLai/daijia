d2f7cc592be7fe3e2d2066a4de257f75ccfd78f3
laibaijiang <laibaijiang@laibaijiang.com>
2024-08-08 11:38:36 +0800

    增加预估订单数据

--- web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
+++ web/web-customer/src/main/java/com/atguigu/daijia/customer/controller/OrderController.java
@@ -1,9 +1,16 @@
 package com.atguigu.daijia.customer.controller;
 
+import com.atguigu.daijia.common.login.GuiguLogin;
+import com.atguigu.daijia.common.result.Result;
 import com.atguigu.daijia.customer.service.OrderService;
+import com.atguigu.daijia.model.form.customer.ExpectOrderForm;
+import com.atguigu.daijia.model.vo.customer.ExpectOrderVo;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,6 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/order")
 @SuppressWarnings({"unchecked", "rawtypes"})
 public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Operation(summary = "预估订单数据")
+    @GuiguLogin
+    @PostMapping("/expectOrder")
+    public Result<ExpectOrderVo> expectOrder(@RequestBody ExpectOrderForm expectOrderForm) {
+        return Result.ok(orderService.expectOrder(expectOrderForm));
+    }
 
 
 }
